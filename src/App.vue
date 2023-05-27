8dc62429743ff5a473f1cf8d28cdaca845efea32
gen_nakamura <gen0401@keio.jp>
2023-05-27 03:02:58 -0700

    voccy voccy

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
+    <img :src="currentImage" alt="Image" :style="{width: 300}">
     <h1>Welcome to the Vocabulary App</h1>
+    <h1>Let's Voccy!↓</h1>
     <p>Start learning and expanding your vocabulary!</p>
-    <button @click="startLearning">Start Learning</button>
+    <button @click="changeImage">Start Learning</button>
   </div>
 </template>
 
 <script>
 import { createCompletion } from '@/api/api';
+import image1 from '@/assets/afterVoccy.png';
+import image2 from '@/assets/beforeVoccy.webp';
 
 export default {
+  data() {
+    return {
+      currentImage: image2,
+      images: [
+        image2,
+        image1
+      ],
+      currentIndex: 0
+    };
+  },
   // async created() {
     // },
   methods: {
+    changeImage() {
+      this.currentIndex++;
+      if (this.currentIndex >= this.images.length) {
+        this.currentIndex = 0;
+      }
+      this.currentImage = this.images[this.currentIndex];
+    },
     async startLearning() {
       console.log('start learning');
       const prompt = 'MY PROMPT HERE';
