711172dc4bc0c53c60c011e5b5af0fb37212ff1a
gen_nakamura <gen0401@keio.jp>
2023-06-01 15:26:31 -0700

    create answering button

--- src/components/FlashcardsTest.vue
+++ src/components/FlashcardsTest.vue
@@ -1,63 +1,23 @@
 <template>
   <div class="test-container">
-    <div class="question-container">
-      <h1>{{ currentQuestion }}</h1>
-      <button @click="showAnswer">答え</button>
-      <div v-if="showingAnswer" class="answer-container">
-        <h2>{{ currentAnswer }}</h2>
+    <div class="card-container">
+      <div class="card">
+        <div class="card-content">
+          <h1 class="question">{{ currentQuestion }}</h1>
+          <div v-if="showingAnswer" class="answer-container">
+            <h2 class="answer">{{ currentAnswer }}</h2>
+          </div>
+        </div>
       </div>
     </div>
+    <!-- <button @click="showAnswer" class="answer-button">答え</button> -->
     <div class="choices-container">
-      <button @click="submitAnswer(false)">×</button>
-      <button @click="submitAnswer(true)">○</button>
-      <button @click="submitAnswer(true, true)">◎</button>
+      <button @click="submitAnswer(false)" class="choice-button wrong"><i class="fas fa-times fa-2x fa-fw" aria-hidden="true"></i></button>
+      <button @click="submitAnswer(true)" class="choice-button correct"><i class="far fa-circle fa-2x fa-fw" aria-hidden="true"></i></button>
+      <button @click="submitAnswer(true, true)" class="choice-button correct"><i class="fas fa-check-circle fa-2x fa-fw" aria-hidden="true"></i></button>
     </div>
   </div>
 </template>
-
-<script>
-// import axios from 'axios';
-
-export default {
-  data() {
-    return {
-      currentQuestion: 'Sample Question',
-      currentAnswer: 'Sample Answer',
-      showingAnswer: false
-    };
-  },
-  mounted() {
-    this.getQuestion();
-  },
-  methods: {
-    getQuestion() {
-      // axios.get('/api/question')
-      //   .then(response => {
-      //     this.currentQuestion = response.data.question;
-      //     this.currentAnswer = response.data.answer;
-      //     this.showingAnswer = false;
-      //   })
-      //   .catch(error => {
-      //     console.error('Error getting question:', error);
-      //   });
-    },
-    showAnswer() {
-      this.showingAnswer = !this.showingAnswer;
-    },
-    submitAnswer(isCorrect, isPerfect = false) {
-      // フロントエンドからサーバーサイドに回答を送信するロジックを追加する
-      // isCorrect: 正解かどうか（true/false）
-      // isPerfect: 完全正解かどうか（true/false）
-      // サーバーサイドのAPIエンドポイントにリクエストを送信し、回答を保存する
-      // 例：axios.post('/api/answer', { isCorrect, isPerfect })
-      console.log(isCorrect, isPerfect);
-
-      // 新しい問題を取得する
-      this.getQuestion();
-    }
-  }
-};
-</script>
 
 <style scoped>
 .test-container {
@@ -66,23 +26,40 @@ export default {
   align-items: center;
 }
 
-.question-container {
+.card-container {
   margin-top: 20px;
   text-align: center;
 }
 
-.answer-container {
-  margin-top: 20px;
+.card {
+  background-color: #fff;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  padding: 20px;
 }
 
-.choices-container {
+.card-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.question {
+  margin-bottom: 20px;
+}
+
+.answer-container {
   margin-top: 20px;
   display: flex;
   justify-content: center;
 }
 
-button {
-  margin: 0 10px;
+.answer {
+  font-size: 16px;
+}
+
+.answer-button {
+  margin-top: 10px;
   padding: 10px 20px;
   background-color: #4caf50;
   color: #fff;
@@ -91,7 +68,65 @@ button {
   cursor: pointer;
 }
 
-button:hover {
+.answer-button:hover {
   background-color: #45a049;
 }
+
+.choices-container {
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+}
+
+.choice-button {
+  margin: 0 10px;
+  padding: 10px 20px;
+  color: #333;
+  background-color: white;
+  border: 0.5px solid #ccc;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.choice-button:hover {
+  background-color: #ebebeb;
+}
+
+.correct {
+  color: #18AE79;
+}
+
+.wrong {
+  color: #D05700;
+}
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      currentQuestion: 'Sample Question',
+      currentAnswer: 'Sample Answer',
+      showingAnswer: false
+    };
+  },
+  methods: {
+    showAnswer() {
+      this.showingAnswer = !this.showingAnswer;
+    },
+    submitAnswer(isCorrect, isPerfect = false) {
+      console.log(isCorrect, isPerfect);
+      this.getQuestion();
+    },
+    getQuestion() {
+      // データ取得のロジックを追加する
+      this.currentQuestion = 'Sample Question';
+      this.currentAnswer = 'Sample Answer';
+      this.showingAnswer = false;
+    }
+  },
+  mounted() {
+    this.getQuestion();
+  }
+};
+</script>
