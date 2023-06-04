ddb6ac4c3a26eed346f15223c6d2f7814f9f7334
gen_nakamura <gen0401@keio.jp>
2023-06-04 16:36:12 -0700

    create db accessing server endpoints

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
-import { doSomethingAsync, startTest } from './db';
+import { addANewVocab, changeTheSettings, doSomethingAsync, openTheApp, openTheFlashcardsTest, startTest, takeTheFlachcardsTest } from './db';
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+
 
 console.log('server.jsだよおおおおおおお\n\n\n');
 
@@ -15,20 +16,109 @@ export function createServer() {
         res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
         next();
     });
+    
+    // server.post('/api/data', async (req, res) => {
+    //     console.log('get a post request');
+    //     const { questionInput, answerInput } = req.body;
+    //     try {
+    //         await startTest();
+    //         res.json({ success: true });
+    //     } catch (error) {
+    //         console.log('error in db operation: ', error);
+    //         res.status(500).json({ error: 'Internal Server Error' });
+    //     }
+    // });
 
-    server.post('/api/data', async (req, res) => {
-        console.log('get a post request');
-        const { questionInput, answerInput } = req.body;
+    server.post('/api/open_app', async (req, res) => {
+        console.log('/api/open_app');
         try {
-            await startTest();
+            await openTheApp();
             res.json({ success: true });
         } catch (error) {
-            console.log('error in db operation: ', error);
+            console.error('error in /api/open_app: ', error);
             res.status(500).json({ error: 'Internal Server Error' });
         }
     });
 
-    // get quiz from 
-    
+    server.post('/api/add_vocab', async (req, res) => {
+        console.log('/api/add_vocab');
+        const { question, answer } = req.body;
+        try {
+            await addANewVocab(question, answer);
+            res.json({ success: true });
+        } catch (error) {
+            console.error('error in /api/add_vocab: ', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    });
+
+    server.post('/api/change_settings', async (req, res) => {
+        console.log('/api/change_settings');
+        try {
+            await changeTheSettings(req.body);
+            res.json({ success: true });
+        } catch (error) {
+            console.error('error in /api/change_settings: ', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    });
+
+    server.post('/api/open_test', async (req, res) => {
+        console.log('/api/open_test');
+        try {
+            const data = await openTheFlashcardsTest();
+            res.json({ success: true, data: data });
+        } catch (error) {
+            console.error('error in /api/open_test: ', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    });
+
+    server.post('/api/take_test', async (req, res) => {
+        console.log('/api/take_test');
+        try {
+            await takeTheFlachcardsTest(req.body);
+            res.json({ success: true });
+        } catch (error) {
+            console.error('error in /api/take_test: ', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    });
+
+    // server.post('/api/done_test', async (req, res) => {
+    //     console.log('/api/done_test');
+    //     const { questionInput, answerInput } = req.body;
+    //     try {
+    //         await addANewVocab(questionInput, answerInput);
+    //         res.json({ success: true });
+    //     } catch (error) {
+    //         console.error('error in /api/open_app: ', error);
+    //         res.status(500).json({ error: 'Internal Server Error' });
+    //     }
+    // });
+
+    // server.post('/api/on_notify', async (req, res) => {
+    //     console.log('/api/on_notify');
+    //     const { questionInput, answerInput } = req.body;
+    //     try {
+    //         await addANewVocab(questionInput, answerInput);
+    //         res.json({ success: true });
+    //     } catch (error) {
+    //         console.error('error in /api/open_app: ', error);
+    //         res.status(500).json({ error: 'Internal Server Error' });
+    //     }
+    // });
+
+    // server.post('/api/open_config', async (req, res) => {
+    //     console.log('/api/open_config');
+    //     const { questionInput, answerInput } = req.body;
+    //     try {
+    //         await addANewVocab(questionInput, answerInput);
+    //         res.json({ success: true });
+    //     } catch (error) {
+    //         console.error('error in /api/open_app: ', error);
+    //         res.status(500).json({ error: 'Internal Server Error' });
+    //     }
+    // });
     return server;
 };
