a15334eaffc856b7eda1bde8a3d7d6a44f7c3170
gen_nakamura <gen0401@keio.jp>
2023-06-05 21:00:17 -0700

    set update-electron config

--- src/background.js
+++ src/background.js
@@ -6,7 +6,10 @@ import installExtension, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 import { createServer } from './server/server'
 import axios from 'axios'
-require('update-electron-app')()
+require('update-electron-app')({
+  repo: 'gen-nakamura/Voccy',
+  updateInterval: '5 minutes'
+})
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
